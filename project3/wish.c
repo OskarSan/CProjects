fba43741b493de34f7dd64a7f54fa6e5cc096cb0
OskarSan <oskar.sankila@gmail.com>
2025-01-05 15:12:55 +0200

    small changes

--- project3/wish.c
+++ project3/wish.c
@@ -16,7 +16,8 @@ void handle_alarm(int signo) {
 int main(int argc, char *argv[]) {
  
     int loop = 1;
-    char userInput[255];
+    char *userInput = NULL;
+    size_t len = 0;
     pid_t pid;  
 
     signal(SIGALRM, handle_alarm);
@@ -26,7 +27,7 @@ int main(int argc, char *argv[]) {
 
         alarm(TIMEOUT);
 
-        if (fgets(userInput, sizeof(userInput), stdin) != NULL) {
+        if (getline(&userInput, &len, stdin) != -1) {
             
             alarm(0);
             userInput[strcspn(userInput, "\n")] = '\0';
@@ -53,9 +54,10 @@ int main(int argc, char *argv[]) {
             
             }       
         }
-        printf("\nwish$ ");
+        
     }
 
 
+    free(userInput);
     return 0;
 }
